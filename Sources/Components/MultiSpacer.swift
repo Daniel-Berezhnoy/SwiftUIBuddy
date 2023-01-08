62a525b2f6733178175ccbf2a7f365d4678f8947
Daniel Berezhnoy <43868541+Daniel-Berezhnoy@users.noreply.github.com>
2023-01-07 17:34:08 -0800

    Dang

---
+++ Sources/Components/MultiSpacer.swift
@@ -0,0 +1,22 @@
+//
+//  MultiSpacer.swift
+//  
+//
+//  Created by Daniel Berezhnoy on 1/7/23.
+//
+
+import SwiftUI
+
+@available(iOS 15.0, *)
+public struct MultiSpacer: View {
+    public var body: some View {
+        Text("Maybe come up with a better name!")
+    }
+}
+
+@available(iOS 15.0, *)
+struct MultiSpacer_Previews: PreviewProvider {
+    static var previews: some View {
+        MultiSpacer()
+    }
+}
