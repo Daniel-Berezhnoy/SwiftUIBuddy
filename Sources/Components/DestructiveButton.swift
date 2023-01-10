8444fc7dc617c2e3ba91a8c2b8f83343471b678f
Daniel Berezhnoy <43868541+Daniel-Berezhnoy@users.noreply.github.com>
2023-01-09 20:18:49 -0800

    Added templates for the buttons

---
+++ Sources/Components/DestructiveButton.swift
@@ -0,0 +1,25 @@
+//
+//  DestructiveButton.swift
+//  
+//
+//  Created by Daniel Berezhnoy on 1/9/23.
+//
+
+import SwiftUI
+
+@available(iOS 15.0, *)
+public struct DestructiveButton: View {
+    
+    public var body: some View {
+        Text("Create Destructive Button in Here!")
+    }
+    
+    public init() {}
+}
+
+@available(iOS 15.0, *)
+struct DestructiveButton_Previews: PreviewProvider {
+    static var previews: some View {
+        DestructiveButton()
+    }
+}
---
+++ Sources/Components/StandardButton.swift
@@ -0,0 +1,25 @@
+//
+//  StandardButton.swift
+//  
+//
+//  Created by Daniel Berezhnoy on 1/9/23.
+//
+
+import SwiftUI
+
+@available(iOS 15.0, *)
+public struct StandardButton: View {
+    
+    public var body: some View {
+        Text("Create Standard Button in here")
+    }
+    
+    public init() {}
+}
+
+@available(iOS 15.0, *)
+struct StandardButton_Previews: PreviewProvider {
+    static var previews: some View {
+        StandardButton()
+    }
+}
