c23875050aa2addaf97cd2cc1c2a31c46ef29dcd
Daniel Berezhnoy <43868541+Daniel-Berezhnoy@users.noreply.github.com>
2023-01-09 20:02:08 -0800

    LoadingSpinner done!

--- Sources/Components/LoadingSpinner.swift
+++ Sources/Components/LoadingSpinner.swift
@@ -9,16 +9,40 @@ import SwiftUI
 
 @available(iOS 15.0, *)
 public struct LoadingSpinner: View {
+    let color: Color
+    
     public var body: some View {
-        Text("Hello, World!")
+        ZStack {
+            rectangle
+            spinner
+        }
+        .frame(width: 85, height: 85)
     }
     
-    public init() {}
+    var rectangle: some View {
+        Rectangle()
+            .foregroundStyle(color)
+            .cornerRadius(20)
+            .opacity(0.15)
+    }
+    
+    var spinner: some View {
+        ProgressView()
+            .progressViewStyle(.circular)
+            .controlSize(.large)
+            .tint(color)
+    }
+    
+    /// A loading spinner. Change the tint color for customization.
+    public init(tint: Color = .secondary) {
+        self.color = tint
+    }
 }
 
 @available(iOS 15.0, *)
 struct LoadingSpinner_Previews: PreviewProvider {
     static var previews: some View {
         LoadingSpinner()
+        LoadingSpinner(tint: .blue)
     }
 }
