86c81d5f3220c24d77f7992f7c02151d2185ff18
Daniel Berezhnoy <43868541+Daniel-Berezhnoy@users.noreply.github.com>
2023-01-15 13:58:14 -0800

    Added a Plain DesctiveButtonStyle

--- Sources/Components/DestructiveButton.swift
+++ Sources/Components/DestructiveButton.swift
@@ -14,41 +14,65 @@ public struct DestructiveButton: View {
     let font: Font
     let height: Double
     let action: () -> Void
+    var buttonStyle: DestructiveButtonStyle
     
     public var body: some View {
         Button {
             action()
         } label: {
             ZStack {
-                border
+                background
                 text
             }
         }
         .padding(.horizontal, 25)
     }
     
-    var border: some View {
-        Rectangle()
-            .frame(height: height)
-            .foregroundColor(.background)
-            .overlay(RoundedRectangle(cornerRadius: 10)
-                .stroke(lineWidth: 2)
-                .foregroundColor(.red))
+    var background: some View {
+        ZStack {
+            if buttonStyle == .bordered {
+                Rectangle()
+                    .frame(height: height)
+                    .foregroundColor(.background)
+                    .overlay(RoundedRectangle(cornerRadius: 10)
+                        .stroke(lineWidth: 2)
+                        .foregroundColor(.red))
+                
+            } else {
+                Rectangle()
+                    .frame(height: height)
+                    .foregroundColor(.red)
+                    .cornerRadius(10)
+            }
+        }
     }
     
     var text: some View {
-        Text(title)
-            .font(.system(size: 18, weight: .semibold, design: .rounded))
-            .fontWeight(.semibold)
-            .foregroundColor(.red)
+        ZStack {
+            if buttonStyle == .bordered {
+                Text(title)
+                    .font(.system(size: 18, weight: .semibold, design: .rounded))
+                    .fontWeight(.semibold)
+                    .foregroundColor(.red)
+            } else {
+                Text(title)
+                    .font(.system(size: 18, weight: .semibold, design: .rounded))
+                    .fontWeight(.semibold)
+                    .foregroundColor(.white)
+            }
+        }
     }
     
+    public enum DestructiveButtonStyle { case plain, bordered }
+    
     public init(title: String,
+                style: DestructiveButtonStyle = .bordered,
                 font: Font = Font.system(size: 18, weight: .semibold, design: .rounded),
                 height: Double = 55,
                 action: @escaping () -> Void) {
         
         self.title = title
+        self.buttonStyle = style
         self.font = font
         self.height = height
         self.action = action
@@ -59,5 +83,6 @@ public struct DestructiveButton: View {
 struct DestructiveButton_Previews: PreviewProvider {
     static var previews: some View {
         DestructiveButton(title: "Delete Account") {}
+        DestructiveButton(title: "Remove Connection", style: .plain) {}
     }
 }
