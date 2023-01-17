937e7f2db04a9c24f005595d49f9db341e41cd84
Daniel Berezhnoy <43868541+Daniel-Berezhnoy@users.noreply.github.com>
2023-01-17 12:57:00 -0800

    Save

--- Sources/Components/DynamicTextField.swift
+++ Sources/Components/DynamicTextField.swift
@@ -21,5 +21,78 @@ struct DynamicTextField: View {
 struct DynamicTextField_Previews: PreviewProvider {
     static var previews: some View {
         DynamicTextField()
+//        AdaptiveTextField(title: "Hello",
+//                          text: .constant("Hi"),
+//                          isPassword: true,
+//                          passwordVisible: .constant(false),
+//                          autocapitalization: .never)
+    }
+}
+
+@available(iOS 15.0, *)
+struct TextFieldBorderOverlay: View {
+    
+    var presented: Bool
+    var title: String
+    
+    var body: some View {
+        if presented {
+            Text(title)
+                .font(Font.custom("Montserrat", size: 13))
+                .padding(6)
+                .background(Color.background)
+                .offset(x: 15, y: -15)
+        }
+    }
+}
+
+@available(iOS 15.0, *)
+struct AdaptiveTextField: View {
+    
+    @Binding var passwordVisible: Bool
+    @Binding var text: String
+    
+    let autocapitalization: TextInputAutocapitalization
+    let isPassword: Bool
+    let title: String
+    
+    var body: some View {
+        if isPassword { passwordField } else { loginField }
+    }
+    
+    var passwordField: some View {
+        ZStack {
+            if passwordVisible {
+                TextField(title, text: $text)
+            } else {
+                SecureField(title, text: $text)
+            }
+        }
+        .textInputAutocapitalization(.never)
+        .disableAutocorrection(true)
+        .font(Font.custom("Montserrat", size: 17))
+        .padding(.leading)
+        .padding(.vertical)
+    }
+    
+    var loginField: some View {
+        TextField(title, text: $text)
+            .textInputAutocapitalization(autocapitalization)
+            .disableAutocorrection(true)
+            .font(Font.custom("Montserrat", size: 17))
+            .padding()
+    }
+    
+    init(title: String,
+         text: Binding<String>,
+         isPassword: Bool,
+         passwordVisible: Binding<Bool>,
+         autocapitalization: TextInputAutocapitalization) {
+        
+        _text = text
+        self.title = title
+        self.isPassword = isPassword
+        _passwordVisible = passwordVisible
+        self.autocapitalization = autocapitalization
     }
 }
