58609649cc018a1180bd76ff23b87723596037d8
Daniel Berezhnoy <43868541+Daniel-Berezhnoy@users.noreply.github.com>
2023-01-17 23:05:21 -0800

    Created an empty file

---
+++ Sources/Components/SwiftUIView.swift
@@ -0,0 +1,24 @@
+//
+//  SwiftUIView.swift
+//  
+//
+//  Created by Daniel Berezhnoy on 1/17/23.
+//
+
+import SwiftUI
+
+#warning("Working Here")
+@available(iOS 15.0, *)
+public struct SwiftUIView: View {
+    
+    public var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+@available(iOS 15.0, *)
+struct SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIView()
+    }
+}
