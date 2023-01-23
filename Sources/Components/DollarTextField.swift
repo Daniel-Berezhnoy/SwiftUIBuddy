e1f45da76f69c1114c62c082b878b340e34a5419
Daniel Berezhnoy <43868541+Daniel-Berezhnoy@users.noreply.github.com>
2023-01-22 17:09:52 -0800

    Added 2 initializers

--- Sources/Components/DollarTextField.swift
+++ Sources/Components/DollarTextField.swift
@@ -12,24 +12,34 @@ public struct DollarTextField: View {
     
     @Binding var amount: Double
     
+    let font: Font
     let size: CGFloat
-    let placeholder: Placeholder
-    
     let design: Font.Design
-    let alignment: TextAlignment
     
+    let alignment: TextAlignment
+    let placeholder: Placeholder
     
     public var body: some View {
         TextField("$\(placeholder)", value: $amount, format: .currency(code: "USD"))
-        
-        
-            .font(.system(size: size, weight: .semibold, design: design))
-        
-        
             .multilineTextAlignment(alignment)
             .keyboardType(.decimalPad)
             .minimumScaleFactor(0.9)
             .lineLimit(1)
+            .font(font)
+    }
+    
+    public init(_ placeholder: String,
+                amount: Binding <Double>,
+                font: Font = .system(size: 50, weight: .semibold, design: .rounded),
+                alignment: TextAlignment = .center) {
+        
+        _amount = amount
+        self.font = font
+        self.alignment = alignment
+        self.placeholder = placeholder
+        
+        self.size = 50
+        self.design = .default
     }
     
     public init(_ placeholder: String,
@@ -41,19 +51,31 @@ public struct DollarTextField: View {
         _amount = amount
         self.size = size
         self.design = design
+        self.font = .system(size: size, weight: .semibold, design: design)
         self.alignment = alignment
         self.placeholder = placeholder
     }
     
-    public typealias Placeholder = String
+    typealias Placeholder = String
 }
 
 @available(iOS 15.0, *)
 struct DollarTextField_Previews: PreviewProvider {
     static var previews: some View {
         VStack(spacing: 30) {
-            DollarTextField("4.50", amount: .constant(5))
-            DollarTextField("3", amount: .constant(500), size: 25, design: .monospaced)
+//            DollarTextField("4.50", amount: .constant(5))
+//            DollarTextField("3", amount: .constant(500), size: 16)
+            
+            DollarTextField("33.30",
+                            amount: .constant(5),
+                            size: 60,
+                            design: .rounded,
+                            alignment: .center)
+            
+            DollarTextField("22.20",
+                            amount: .constant(5),
+                            font: .system(size: 30, weight: .light, design: .monospaced),
+                            alignment: .center)
         }
     }
 }
