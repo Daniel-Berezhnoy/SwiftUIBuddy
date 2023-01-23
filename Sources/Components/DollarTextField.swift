89b0dcbc18121136e804103f6da0864d97be66a7
Daniel Berezhnoy <43868541+Daniel-Berezhnoy@users.noreply.github.com>
2023-01-22 16:01:12 -0800

    Added DollarTextField

---
+++ Sources/Components/DollarTextField.swift
@@ -0,0 +1,25 @@
+//
+//  DollarTextField.swift
+//  
+//
+//  Created by Daniel Berezhnoy on 1/22/23.
+//
+
+import SwiftUI
+
+@available(iOS 15.0, *)
+public struct DollarTextField: View {
+    
+    public var body: some View {
+        Text("Hello, World!")
+    }
+    
+    public init() {}
+}
+
+@available(iOS 15.0, *)
+struct DollarTextField_Previews: PreviewProvider {
+    static var previews: some View {
+        DollarTextField()
+    }
+}
