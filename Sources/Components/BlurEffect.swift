7906d355ca5959797add767368cf32bac5134859
Daniel Berezhnoy <43868541+Daniel-Berezhnoy@users.noreply.github.com>
2023-01-28 04:10:33 -0800

    Made some progress on BlurEffect

--- Sources/Components/BlurEffect.swift
+++ Sources/Components/BlurEffect.swift
@@ -10,17 +10,57 @@ import SwiftUI
 @available(iOS 15.0, *)
 struct BlurEffect: View {
     
+    @State private var blurEnabled = true
+    
     public var body: some View {
         ZStack {
+            testContent
+            .blur(radius: blurEnabled ? 5 : 0)
+            
+            showContentButton
+        }
+    }
+    
+    var testContent: some View {
+        ZStack {
             Rectangle()
-                .frame(width: 200, height: 300)
                 .foregroundStyle(.blue)
                 .cornerRadius(20)
             
-            Text("Do you see me?")
+            Text("The company confirmed on Friday that Twitter users accessing the social media platform on Android, iOS, and the web can now add content warnings to any photos or videos attached to their tweets.")
+                .padding()
                 .foregroundStyle(.white)
         }
-        .blur(radius: 10)
+        .frame(width: 200, height: 300)
+    }
+    
+    var showContentButton: some View {
+        Button {
+            withAnimation { blurEnabled.toggle() }
+        } label: {
+            ZStack { if blurEnabled { showContentLabel } else { EmptyView() } }
+                .foregroundStyle(.white)
+                .padding()
+        }
+    }
+    
+    var showContentLabel: some View {
+        ZStack {
+            if #available(iOS 16.0, *) {
+                Label("Show Content", systemImage: "eye.slash.fill")
+                    .fontWeight(.semibold)
+            } else {
+                HStack {
+                    Image(systemName: "eye.slash.fill")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 25)
+                    
+                    Text("Show Content")
+                        .fontWeight(.semibold)
+                }
+            }
+        }
     }
     
     public init() {}
