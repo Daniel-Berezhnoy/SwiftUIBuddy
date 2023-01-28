7fb19cf033d8bb4c323c2dc875eeefd0627dcf50
Daniel Berezhnoy <43868541+Daniel-Berezhnoy@users.noreply.github.com>
2023-01-28 05:00:57 -0800

    BlurFilter is working

--- Sources/Components/BlurFilter.swift
+++ Sources/Components/BlurFilter.swift
@@ -7,16 +7,43 @@
 
 import SwiftUI
 
-@available(iOS 13.0, *)
+@available(iOS 15.0, *)
 struct BlurFilter: View {
+    
     public var body: some View {
-        Text("Hello, World!")
+        ZStack {
+            GeometryReader { proxy in
+                let frame = proxy.frame(in: .global)
+                
+                Image(systemName: "swift")
+                    .resizable()
+                    .scaledToFit()
+                    .padding()
+                    .frame(width: frame.size.width, height: frame.size.height)
+            }
+            
+            GeometryReader { proxy in
+                BlurView(style: .dark)
+            }
+        }
+        .ignoresSafeArea()
     }
     
     public init() {}
 }
 
-@available(iOS 13.0, *)
+@available(iOS 15.0, *)
+struct BlurView: UIViewRepresentable {
+    let style: UIBlurEffect.Style
+    
+    func makeUIView(context: Context) -> UIVisualEffectView {
+        UIVisualEffectView(effect: UIBlurEffect(style: style))
+    }
+    
+    func updateUIView(_ uiView: UIVisualEffectView, context: Context) {}
+}
+
+@available(iOS 15.0, *)
 struct BlurFilter_Previews: PreviewProvider {
     static var previews: some View {
         BlurFilter()
