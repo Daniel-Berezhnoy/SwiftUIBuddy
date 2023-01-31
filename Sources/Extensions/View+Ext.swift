50a7a1cf86a60e6703cc55715bd2c2e49fa11b0b
Daniel Berezhnoy <43868541+Daniel-Berezhnoy@users.noreply.github.com>
2023-01-31 13:54:36 -0800

    Completely killing BlurFilter - Can't find a viable solution

--- Sources/Extensions/View+Ext.swift
+++ Sources/Extensions/View+Ext.swift
@@ -10,25 +10,6 @@ import SwiftUI
 @available(iOS 15.0, *)
 extension View {
      
-    public func blurFilter(blurStyle: UIBlurEffect.Style = .regular, timer: Int = 5) -> some View {
-        
-        var viewWidth = 0.00
-        var viewHeight = 0.00
-        
-        return ZStack {
-            self.readSize { size in
-                viewWidth = size.width
-                viewHeight = size.height
-                
-                print("View Width: \(viewWidth)")
-                print("View Height: \(viewHeight) \n")
-            }
-            
-            BlurFilter(blurStyle: blurStyle, timer: timer)
-        }
-        .frame(width: viewWidth, height: viewHeight)
-    }
-
     func readSize(onChange: @escaping (CGSize) -> Void) -> some View {
         background(
             GeometryReader { geometryProxy in
