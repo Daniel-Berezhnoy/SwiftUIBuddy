65b1b8c63fc8d59550232ac5b5c6d6b46504ab68
Daniel Berezhnoy <43868541+Daniel-Berezhnoy@users.noreply.github.com>
2023-01-31 12:36:10 -0800

    Updated blurFiler() to use the readSize()

--- Sources/Extensions/View+Ext.swift
+++ Sources/Extensions/View+Ext.swift
@@ -9,12 +9,25 @@ import SwiftUI
 
 @available(iOS 15.0, *)
 extension View {
-    
+     
     public func blurFilter(blurStyle: UIBlurEffect.Style = .regular, timer: Int = 5) -> some View {
-        ZStack {
-            self
+        
+        var viewWidth = 0.00
+        var viewHeight = 0.00
+        
+        return ZStack {
+            self.readSize { size in
+                viewWidth = size.width
+                viewHeight = size.height
+                    
+                print("View Width: \(viewWidth)")
+                print("View Height: \(viewHeight) \n")
+            }
+            
             BlurFilter(blurStyle: blurStyle, timer: timer)
+               
         }
+        .frame(width: viewWidth, height: viewHeight)
     }
 
     func readSize(onChange: @escaping (CGSize) -> Void) -> some View {
